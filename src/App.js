0dd0a8e51046b55f15bf4cd8cb628b05423af674
Ray King <taipan.king@gmail.com>
2018-03-30 10:50:09 +1100

    chore(webpack/react): Add webpack build config for and with react.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const App = () => {
+  return (<div><p>Delivered Via React !</p></div>)
+};
+
+ReactDOM.render(<App/>, document.getElementById("app"));
+
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import App from "./App";
+import style from "./main.css";
+const numList = [1,2,3,4,5,6,7,8,9];
+const es6Out = () => console.log(...numList);
+
+
+
+es6Out();
