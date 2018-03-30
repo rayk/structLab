f350ad0e4e83c716600ec67a2a2947bdaa1584db
Ray King <taipan.king@gmail.com>
2018-03-30 11:45:23 +1100

    chore(eslint): Add eslint config.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export const App = () => {
-  return (<div><p>Delivered Via React !</p></div>)
+  return (
+    <div>
+      <p>Delivered Via React !</p>
+    </div>
+  );
 };
 
-ReactDOM.render(<App/>, document.getElementById("app"));
-
-
+ReactDOM.render(<App />, document.getElementById("app"));
